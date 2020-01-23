1153baa721dc267454728f24b04c0f327a7be143
desiba-1 <dayodeji@aellacredit.com>
2020-01-23 19:17:36 +0100

    login feature

--- routes/index.js
+++ routes/index.js
@@ -133,7 +133,7 @@ router.get('/webhook', (req, res) => {
  
   // Your verify token. Should be a random string.
   let VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
-
+/*
   // Parse the query params
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
@@ -154,14 +154,65 @@ router.get('/webhook', (req, res) => {
           res.sendStatus(403);
       }
   }
+  */
 });
 
 router.post('/webhook', (req, res) => {
 
-  var data = req.body;
-  console.log(data);
+  // Parse the request body from the POST
+  let body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the body of the webhook event
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+
+     // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);        
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
+      
+    });
+
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
     
 });
+
+
+function handleMessage(sender_psid, received_message) {
+
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {    
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }  
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response);    
+}
 
 
 const handlePostback = (sender_psid, received_postback) => {
@@ -177,70 +228,7 @@ const handlePostback = (sender_psid, received_postback) => {
 }
 
 
-const askTemplate = (text) => {
-  return {
-      "attachment":{
-          "type":"template",
-          "payload":{
-              "template_type":"button",
-              "text": text,
-              "buttons":[
-                  {
-                      "type":"postback",
-                      "title":"Cats",
-                      "payload":"CAT_PICS"
-                  },
-                  {
-                      "type":"postback",
-                      "title":"Dogs",
-                      "payload":"DOG_PICS"
-                  }
-              ]
-          }
-      }
-  }
-}
 
-function handleDialogFlowResponse(sender, response) {
-  let responseText = response.fulfillmentMessages.fulfillmentText;
-
-  let messages = response.fulfillmentMessages;
-  let action = response.action;
-  let contexts = response.outputContexts;
-  let parameters = response.parameters;
-
-  //sendTypingOff(sender);
-
-  if (isDefined(messages)) {
-      handleMessages(messages, sender);
-  } else if (isDefined(responseText)) {
-    sendTextMessage(sender, responseText);
-  }
-}
-
-function handleMessages(message, sender) {
-  switch (message.message) {
-      case "text": //text
-          message.text.text.forEach((text) => {
-              if (text !== '') {
-                  sendTextMessage(sender, text);
-              }
-          });
-          break;
-  }
-}
-
-function sendTextMessage(recipientId, text) {
-  var messageData = {
-      recipient: {
-          id: recipientId
-      },
-      message: {
-          text: text
-      }
-  }
-  callSendAPI(messageData);
-}
 
 
 function callSendAPI(sender_psid, response) {
